aab47aeef4e3251a05b7fad8ec683ef17e606dd0
nashkevich <agz-2001@mail.ru>
2025-02-12 12:40:21 +0100

    add basket update

--- user/user_views.py
+++ user/user_views.py
@@ -2,6 +2,7 @@ from rest_framework import generics,views
 from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 from django.contrib.auth.models import User
 from .models import LikedMoviesUser
 from .serializers import UserRegistrationSerializer,UserGetSerializer,BasketSerializer
@@ -27,9 +28,17 @@ class GetUserView(views.APIView):
         return Response({'message':"Get user data",'response':serializer.data},status=200)
     
 class BasketUserView(views.APIView):
-    permission_classes = [IsAuthenticated]
     def get(self,request,*args,**kwargs):
         userId = request.GET.get('userId')
         basket = LikedMoviesUser.objects.get(user__id=userId)
         serializer = BasketSerializer(basket)
         return Response({'message':"Get basket for user!",'response':serializer.data},status=200)
+    
+    def put(self,request,*args,**kwargs):
+        data = request.data
+        user = request.user
+        serializer_user = UserGetSerializer(user)
+        basket_object = LikedMoviesUser.objects.get(user_id=serializer_user.data['id'])
+        basket_object.basket = data.get('basket')
+        basket_object.save()
+        return Response({"message":"Success updated basket object!"},status=200)
